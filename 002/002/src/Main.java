bb705bfbd24cda1d6f0de56e184df1dcd0e361a4
Felipe Costa <felipecostasiqu@gmail.com>
2025-06-25 18:54:44 -0300

    :tada: Exercise 002

--- 002/002/src/Main.java
+++ 002/002/src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        
+        // Integer
+        byte _numberByte = 1;
+        short _numberShort = 1;
+        int _numberInt = 1;
+        long _numberLong = 1L;
+
+        // Decimals
+        float _numberFloat = 1.0f;
+        double _numberDouble = 1.0;
+
+        // Characters
+        String _string = "Hello, World!";
+        char _stringChar = 'A';
+
+        // Boolean
+        boolean _boolean = true;
+
     }
 }
